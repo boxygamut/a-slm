5684e04045b3d33605a630f8182e1b361658895a
boxygamut <95462626+boxygamut@users.noreply.github.com>
2026-08-08 02:51:48 -0700

    Finished RoPE implementation

--- transformer.py
+++ transformer.py
@@ -17,8 +17,49 @@ class MLP(nn.Module): # Add gated projection and use SwiGLU
         return self.net(x)
 
 
+def get_rope_frequencies(head_size, theta, device = None):
+    pair_indices = torch.arange(
+        head_size // 2,
+        device = device
+    )
+
+    return theta ** (-2 * pair_indices / head_size)
+
+def apply_rope(x, theta = 10000):
+    B, H, T, D = x.shape # shape post permute in the GQA section
+
+    freqs = get_rope_frequencies(
+        D,
+        theta,
+        device = x.device
+    )
+
+    positions = torch.arange(
+        T,
+        device = x.device
+    )
+
+    angles = positions[:, None] * freqs[None, :] # Column positions * row freqs
+
+    cos = torch.cos(angles)
+    sin = torch.sin(angles)
+
+    x_pairs = x.reshape(B, H, T, D // 2, 2)
+    
+    x_even = x_pairs[..., 0] # [B, H, T, D / 2]
+    x_odd = x_pairs[..., 1] # Same as above
+
+    cos = cos[None, None, ...] # [T, D / 2] -> [1, 1, T, D / 2]
+    sin = sin[None, None, ...]
+
+    rotated_even = x_even * cos - x_odd * sin
+    rotated_odd = x_even * sin + x_odd * cos
+
+    return torch.stack((rotated_even, rotated_odd), dim = -1).reshape(B, H, T, D)
+
+
 class GroupedQueryAttention(nn.Module):
-    def __init__(self, hidden_size, num_kv_heads = 4, num_q_heads = 12, window_size = None):
+    def __init__(self, hidden_size, num_kv_heads = 4, num_q_heads = 12, window_size = None, rope_theta = 10000):
         super().__init__()
 
         self.hidden_size = hidden_size
@@ -27,6 +68,7 @@ class GroupedQueryAttention(nn.Module):
         self.num_q_heads = num_q_heads
 
         self.window_size = window_size
+        self.rope_theta = rope_theta
 
         self.queries = nn.Linear(
             hidden_size,
@@ -52,7 +94,8 @@ class GroupedQueryAttention(nn.Module):
             bias = False
         )
 
-        self.rms_norm = nn.RMSNorm(hidden_size)
+        self.q_norm = nn.RMSNorm(self.head_size)
+        self.k_norm = nn.RMSNorm(self.head_size)
 
     def create_mask(self, T, device):
         positions = torch.arange(T, device = device)
@@ -69,11 +112,9 @@ class GroupedQueryAttention(nn.Module):
         mask = mask.masked_fill(~allowed, float("-inf"))
 
         return mask
-
+    
     def forward(self, x):
         B, T, C = x.shape
-
-        x = self.rms_norm(x)
         
         q = (
             self.queries(x)
@@ -86,6 +127,12 @@ class GroupedQueryAttention(nn.Module):
             .reshape(B, T, self.num_kv_heads, self.head_size) # [B, T, 4, 128]
             .permute(0, 2, 1, 3) # [B, 4, T, 128]
         )
+
+        q = self.q_norm(q)
+        k = self.k_norm(k)
+
+        q = apply_rope(q, self.theta)
+        k = apply_rope(k, self.theta)
     
         v = (
             self.val(x)
